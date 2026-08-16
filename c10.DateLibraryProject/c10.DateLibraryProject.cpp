a9be04cdccf64a86606020e75672e3d5cd6b7388
rawand <rawandm771@gmail.com>
2026-08-16 14:44:22 +0300

    Add project files.

---
+++ c10.DateLibraryProject/c10.DateLibraryProject.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include "clsDate.h";
+using namespace std;
+
+int main()
+{
+	
+	clsDate Date1;
+	Date1.Print();
+	clsDate Date2("31/1/2022");
+	Date2.Print();
+	clsDate Date3(20, 12, 2022);
+	Date3.Print();
+	clsDate Date4(250, 2022);
+	Date4.Print();
+	Date1.IncreaseDateByOneMonth();
+	Date1.Print();
+	Date3.PrintMonthCalendar();
+}
